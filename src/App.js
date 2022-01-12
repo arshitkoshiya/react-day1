6d96df53e33aae74b4e637644d35a5ccdfd9d623
AK <arshit.n.p.8@gmail.com>
2022-01-12 16:16:06 +0530

    add loginform

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="form">
+        <form action="#" method="get">
+
+            <div className="heading">
+                <label>AK GAMMING LOGIN FORM</label>
+            </div>
+
+
+            <div className="fillup">
+                <table border="0">
+                    <tr>
+                        <td>
+                            <label htmlFor="">E-mail</label>
+                        </td>
+                        <td>
+                            <input type="email" name="email" id="email" placeholder="ak_gamming_08@Email.com " required/>
+                        </td>
+                        <td>
+                            <label htmlFor="">Password</label>
+                        </td>
+                        <td>
+                            <input type="password" name="" id="password" placeholder="enter a strong password" required/>
+                        </td>
+                        <td>
+                            <button onclick="js()">SUBBMIT</button>
+
+                        </td>
+                    </tr>
+                </table>
+            </div>
+
+        </form>
+    </div>
     </div>
   );
 }
